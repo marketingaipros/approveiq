e64080028b81684c20dfb65eadc2bb9a265ffd16
dwhite538 <dwhitesvp@gmail.com>
2026-03-20 00:58:45 -0500

    Update proxy.ts to import from relocated middleware.ts

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,5 @@
 import { type NextRequest } from 'next/server'
-import { updateSession } from '@/lib/supabase/middleware'
+import { updateSession } from './middleware'
 
 export const runtime = 'edge'
 
